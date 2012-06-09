45f32f85eecbdaca1108542adc9733e9b633d9e5
Bardur Arantsson <bardur@scientician.net>
2012-06-09 17:22:52 +0200

    Lua: Remove dead help code

--- lib/mods/theme/scpt/help.lua
+++ lib/mods/theme/scpt/help.lua
@@ -15,19 +15,3 @@ ingame_help
 		"This can lead to quests. You can also give items to 'monsters' with the y key.",
 	}
 }
-
-ingame_help
-{
-	["no_test"] =   TRUE,
-	["callback"] =  "select_context",
-	["fct"] =       function(typ, name)
-			-- list of files for classes, { filename, anchor }
-			local t =
-			{
-			}
-
-			if t[typ][name] then ingame_help_doc(t[typ][name][1], t[typ][name][2])
-			else ingame_help_doc("help.hlp", 0)
-			end
-	end,
-}
