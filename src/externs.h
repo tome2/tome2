23edf9d415c28b7553f3f86a0a02e146c5a9e210
Bardur Arantsson <bardur@scientician.net>
2015-02-23 09:11:59 +0100

    Remove redundant declaration

--- src/externs.h
+++ src/externs.h
@@ -558,7 +558,6 @@ extern void do_cmd_give(void);
 extern bool_ do_cmd_tunnel_aux(int y, int x, int dir);
 extern void do_cmd_tunnel(void);
 extern void do_cmd_disarm(void);
-extern void do_cmd_disarm(void);
 extern void do_cmd_bash(void);
 extern void do_cmd_alter(void);
 extern void do_cmd_spike(void);
