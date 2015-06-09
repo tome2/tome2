48a5a300cf33278847f64977cac7d86d88997a9d
Bardur Arantsson <bardur@scientician.net>
2015-06-09 20:42:27 +0200

    Fix missing "std::" qualified in make_array()

--- src/include/tome/make_array.hpp
+++ src/include/tome/make_array.hpp
@@ -5,7 +5,7 @@
 /*
  * Make an array of a POD type.
  */
-template <typename T> T *make_array(size_t n) {
+template <typename T> T *make_array(std::size_t n) {
 	static_assert(std::is_pod<T>::value, "Type parameter must be POD type");
 	T *array = new T[n];
 	memset(array, 0, n*sizeof(T));
