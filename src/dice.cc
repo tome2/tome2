4665f2477ba66f303f764f953accc1c798f85d03
Bardur Arantsson <bardur@scientician.net>
2019-02-15 19:20:25 +0100

    Replace uses of bool_ in dice.*

--- src/dice.cc
+++ src/dice.cc
@@ -14,47 +14,46 @@ void dice_init(dice_type *dice, long base, long num, long sides)
 	dice->sides = sides;
 }
 
-bool_ dice_parse(dice_type *dice, const char *s)
+static bool dice_parse(dice_type *dice, const char *s)
 {
 	long base, num, sides;
 
 	if (sscanf(s, "%ld+%ldd%ld", &base, &num, &sides) == 3)
 	{
 		dice_init(dice, base, num, sides);
-		return TRUE;
+		return true;
 	}
 
 	if (sscanf(s, "%ld+d%ld", &base, &sides) == 2)
 	{
 		dice_init(dice, base, 1, sides);
-		return TRUE;
+		return true;
 	}
 
 	if (sscanf(s, "d%ld", &sides) == 1)
 	{
 		dice_init(dice, 0, 1, sides);
-		return TRUE;
+		return true;
 	}
 
 	if (sscanf(s, "%ldd%ld", &num, &sides) == 2)
 	{
 		dice_init(dice, 0, num, sides);
-		return TRUE;
+		return true;
 	}
 
 	if (sscanf(s, "%ld", &base) == 1)
 	{
 		dice_init(dice, base, 0, 0);
-		return TRUE;
+		return true;
 	}
 
-	return FALSE;
+	return false;
 }
 
 void dice_parse_checked(dice_type *dice, const char *s)
 {
-	bool_ result = dice_parse(dice, s);
-	if (!result)
+	if (!dice_parse(dice, s))
 	{
 		abort();
 	}
--- src/dice_fwd.hpp
+++ src/dice_fwd.hpp
@@ -1,12 +1,9 @@
 #pragma once
 
-#include "h-basic.hpp"
-
 typedef struct dice_type dice_type;
 struct dice_type;
 
 void dice_init(dice_type *dice, long base, long num, long sides);
-bool_ dice_parse(dice_type *dice, const char *s);
 void dice_parse_checked(dice_type *dice, const char *s);
 long dice_roll(dice_type *dice);
 void dice_print(dice_type *dice, char *buf);
